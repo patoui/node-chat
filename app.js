56e3fc0de05a164ef502ccbbd6318f5972ebf428
Patrique Ouimet <patrique.ouimet@gmail.com>
2020-04-25 14:24:33 -0400

    Added http auth

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
+const basicAuth = require('express-basic-auth');
+
+app.use(basicAuth({
+    users: { 'secret': 'secret' },
+    challenge: true
+}));
 
 server.listen(process.env.PORT || 3000);
 
